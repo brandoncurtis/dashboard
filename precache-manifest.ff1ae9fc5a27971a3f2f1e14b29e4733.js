ffe1808c852e18612f5b256dc540f78ffcf1a8f3
Brandon Curtis <brandon.curtis@gmail.com>
2021-01-13 23:54:15 -0800

    temp1 @ 369f93ce2386347a1314454812896dad7b1b5f4f

---
+++ precache-manifest.ff1ae9fc5a27971a3f2f1e14b29e4733.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f44c226282cbceb595e79f28ebb4c37d",
+    "url": "/dashboard/index.html"
+  },
+  {
+    "revision": "1ff1a61b0cba864354fe",
+    "url": "/dashboard/static/js/2.263b2324.chunk.js"
+  },
+  {
+    "revision": "f6ed52ec71eb28ce7c6d15f83fa2b44c",
+    "url": "/dashboard/static/js/2.263b2324.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0985763b3ff04b1f6765",
+    "url": "/dashboard/static/js/3.b25e5e94.chunk.js"
+  },
+  {
+    "revision": "4d74e7420d468243981a",
+    "url": "/dashboard/static/js/main.d7e4698f.chunk.js"
+  },
+  {
+    "revision": "e088da5d338b091f8f1c",
+    "url": "/dashboard/static/js/runtime-main.9578a287.js"
+  },
+  {
+    "revision": "b58cf5eb323061da6d9d0af7977c5214",
+    "url": "/dashboard/static/media/HarvestRadio.b58cf5eb.mp3"
+  },
+  {
+    "revision": "575896520020fbd0f4ea1674ceda4b3f",
+    "url": "/dashboard/static/media/eth_dai.57589652.svg"
+  },
+  {
+    "revision": "dfbb5e3792d89e39958c3a61081268df",
+    "url": "/dashboard/static/media/eth_usdc.dfbb5e37.svg"
+  },
+  {
+    "revision": "1bcf41fd64af68e2d586c3a005d32f29",
+    "url": "/dashboard/static/media/eth_usdt.1bcf41fd.svg"
+  },
+  {
+    "revision": "de8940ed6afd4037ee2c397de352c9bb",
+    "url": "/dashboard/static/media/eth_wbtc.de8940ed.svg"
+  },
+  {
+    "revision": "50ba3f462c6db64504cb86cc419e677e",
+    "url": "/dashboard/static/media/gif_tractor.50ba3f46.gif"
+  },
+  {
+    "revision": "03f6920767840988f31f595cc59607be",
+    "url": "/dashboard/static/media/sushi_wbtc.03f69207.svg"
+  }
+]);
